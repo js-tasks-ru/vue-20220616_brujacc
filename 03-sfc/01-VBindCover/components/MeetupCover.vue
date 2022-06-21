d5125fbc50d9d3c11ae1f277a76b12e6a7c31d95
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-06-21 20:46:23 +0300

    Solve VBindCover

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    title: String,
+    image: String,
+  },
+  computed: {
+    background() {
+      return `url(${this.image})`;
+    },
+  },
 };
 </script>
 
@@ -14,8 +23,8 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(background),
+    var(--default-cover);
   /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
   display: flex;
   flex-direction: column;
