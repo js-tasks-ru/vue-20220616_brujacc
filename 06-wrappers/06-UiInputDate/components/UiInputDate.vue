580e43325956713da9c44bab5b8fa54d7785a8e5
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-06-27 17:51:56 +0300

    Solve UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,5 @@
 <template>
-  <ui-input v-bind="$attrs" v-model="modelValueProxy" :type="type" @change="onchange($event)">
+  <ui-input v-bind="$attrs" :modelValue="inputVal" :type="type" @change="onchange($event)">
     <template v-for="slotName in Object.keys($slots)" #[slotName]>
       <slot :name="slotName" />
     </template>
@@ -8,6 +8,7 @@
 
 <script>
 import UiInput from './UiInput';
+import { format, addMinutes } from 'date-fns';
 
 export default {
   name: 'UiInputDate',
@@ -24,49 +25,40 @@ export default {
     },
   },
   emits: ['update:modelValue'],
+  data() {
+    return {
+      format,
+      addMinutes,
+    };
+  },
   computed: {
     newDate() {
       return new Date(this.modelValue);
     },
-    modelValueProxy: {
-      get() {
-        switch (this.type) {
-          case 'time':
-            return this.modelValue ? this.formatDate(this.newDate) : '';
-          case 'datetime-local':
-            return this.modelValue ? this.formatDateTime(this.newDate) : '';
-          default:
-            return this.modelValue ? new Date(this.modelValue).toISOString().substr(0, 10) : '';
-        }
-      },
-      set(value) {
-        this.$emit('update:modelValue', value.valueAsNumber);
-      },
+    inputVal() {
+      switch (this.type) {
+        case 'time':
+          return this.modelValue ? this.formatTime(this.newDate) : '';
+        case 'datetime-local':
+          return this.modelValue ? this.formatDateTime(this.newDate) : '';
+        default:
+          return this.modelValue ? this.formatDate(this.newDate) : '';
+      }
     },
   },
   methods: {
     onchange(event) {
       this.$emit('update:modelValue', event.target.valueAsNumber);
     },
-    padTo2Digits(num) {
-      return num.toString().padStart(2, '0');
-    },
     formatDate(date) {
-      const hours = date.getUTCHours();
-      const minutes = date.getUTCMinutes();
-      const seconds = date.getSeconds();
-      return this.$attrs.step && this.$attrs.step % 60 !== 0
-        ? `${this.padTo2Digits(hours)}:${this.padTo2Digits(minutes)}:${this.padTo2Digits(seconds)}`
-        : `${this.padTo2Digits(hours)}:${this.padTo2Digits(minutes)}`;
+      return format(addMinutes(date, date.getTimezoneOffset()), 'yyyy-MM-dd');
+    },
+    formatTime(date) {
+      const variant = this.$attrs.step && this.$attrs.step % 60 !== 0 ? 'HH:mm:ss' : 'HH:mm';
+      return format(addMinutes(date, date.getTimezoneOffset()), variant);
     },
     formatDateTime(date) {
-      return (
-        [date.getFullYear(), this.padTo2Digits(date.getUTCMonth() + 1), this.padTo2Digits(date.getUTCDate())].join(
-          '-',
-        ) +
-        'T' +
-        [this.padTo2Digits(date.getUTCHours()), this.padTo2Digits(date.getUTCMinutes())].join(':')
-      );
+      return format(addMinutes(date, date.getTimezoneOffset()), "yyyy-MM-dd'T'HH:mm");
     },
   },
 };
