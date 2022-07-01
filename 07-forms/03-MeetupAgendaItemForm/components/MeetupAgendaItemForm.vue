1efd5a40ee610589ea2e2571d45442117f43a7c8
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-07-01 15:03:39 +0300

    Update AgendaItemForm

--- 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
@@ -75,7 +75,6 @@ import UiFormGroup from './UiFormGroup';
 import UiInput from './UiInput';
 import UiDropdown from './UiDropdown';
 import { format, addMinutes } from 'date-fns';
-
 const agendaItemTypeIcons = {
   registration: 'key',
   opening: 'cal-sm',
@@ -86,7 +85,6 @@ const agendaItemTypeIcons = {
   afterparty: 'cal-sm',
   other: 'cal-sm',
 };
-
 const agendaItemDefaultTitles = {
   registration: 'Регистрация',
   opening: 'Открытие',
@@ -97,27 +95,21 @@ const agendaItemDefaultTitles = {
   talk: 'Доклад',
   other: 'Другое',
 };
-
 const agendaItemTypeOptions = Object.entries(agendaItemDefaultTitles).map(([type, title]) => ({
   value: type,
   text: title,
   icon: agendaItemTypeIcons[type],
 }));
-
 const talkLanguageOptions = [
   { value: null, text: 'Не указано' },
   { value: 'RU', text: 'RU' },
   { value: 'EN', text: 'EN' },
 ];
-
 export default {
   name: 'MeetupAgendaItemForm',
-
   agendaItemTypeOptions,
   talkLanguageOptions,
-
   components: { UiIcon, UiFormGroup, UiInput, UiDropdown },
-
   props: {
     agendaItem: {
       type: Object,
@@ -169,7 +161,6 @@ export default {
   position: relative;
   padding: 20px 10% 0 16px;
 }
-
 .agenda-item-form__remove-button {
   position: absolute;
   top: 4px;
@@ -182,49 +173,39 @@ export default {
   cursor: pointer;
   transition: 0.2s opacity;
 }
-
 .agenda-item-form__remove-button:hover {
   opacity: 0.6;
 }
-
 .agenda-item-form__row {
   display: flex;
   flex-direction: column;
 }
-
 .agenda-item-form__col + .agenda-item-form__col {
   margin-top: 16px;
 }
-
 .agenda-item-form__col:first-child {
   margin-left: 0;
 }
-
 @media all and (min-width: 992px) {
   .agenda-item-form {
     padding: 28px 25% 4px 24px;
   }
-
   .agenda-item-form__remove-button {
     top: 20px;
     right: 20px;
   }
-
   .agenda-item-form__row {
     flex-direction: row;
     justify-content: space-between;
     margin: 0 -12px;
   }
-
   .agenda-item-form__col {
     flex: 1 1 auto;
     padding: 0 12px;
   }
-
   .agenda-item-form__col + .agenda-item-form__col {
     margin-top: 0;
   }
-
   .agenda-item-form__col:first-child {
     margin-left: 0;
   }
