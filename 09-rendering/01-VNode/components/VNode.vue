94500040a487fe036f18198812659cab19894f6f
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-07-06 20:03:05 +0300

    Solve VNode

--- 09-rendering/01-VNode/components/VNode.vue
+++ 09-rendering/01-VNode/components/VNode.vue
@@ -11,5 +11,8 @@ export default {
       validator: (vnodes) => (Array.isArray(vnodes) ? vnodes : [vnodes]).every((mustBeVNode) => isVNode(mustBeVNode)),
     },
   },
+  render() {
+    return this.vnode;
+  },
 };
 </script>
