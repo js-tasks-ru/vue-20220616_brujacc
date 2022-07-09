36cf5c85276adc0651f0b1bd82a41941b00824c6
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-07-09 11:56:40 +0300

    WIP TemplateRenderer

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,9 +1,8 @@
-<script>
-// import { compile } from 'vue';
+<script lang="jsx">
+import { compile } from 'vue';
 
 export default {
   name: 'TemplateRenderer',
-
   props: {
     template: {
       type: String,
@@ -19,6 +18,20 @@ export default {
       type: [Object, Array],
       default: () => [],
     },
+  },
+
+  watch: {
+    components: {
+      handler(newVal) {
+        this.$options.components = newVal;
+      },
+      immediate: true,
+    },
+  },
+
+  render(...args) {
+    const renderFunction = compile(this.template);
+    return renderFunction.apply(this, args);
   },
 };
 </script>
